9275f49f33e21729f92c633177f58bd688eb6e52
Namratha <Namratha@gbcilsa313grad02imac.dhcp.asu.edu>
2019-09-12 12:52:54 -0700

    [CITE-38] Revert default formatting.

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/concepts/types/AddConceptTypeController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/concepts/types/AddConceptTypeController.java
@@ -18,26 +18,26 @@ public class AddConceptTypeController {
 
     @Autowired
     private IConceptTypeManager conceptTypeManager;
-
+    
     @Autowired
     private IUserManager userManager;
-
-    @RequestMapping(value = "/auth/concepts/types/add", method = RequestMethod.GET)
+    
+    @RequestMapping(value="/auth/concepts/types/add", method=RequestMethod.GET)
     public String show(Model model) {
         model.addAttribute("conceptTypeForm", new ConceptTypeForm());
         return "auth/concepts/types/add";
     }
-
-    @RequestMapping(value = "/auth/concepts/types/add", method = RequestMethod.POST)
+    
+    @RequestMapping(value="/auth/concepts/types/add", method=RequestMethod.POST)
     public String post(ConceptTypeForm form, Model model, Principal principal) {
 
         IUser user = userManager.findByUsername(principal.getName());
-        if (form.getName() != null && !form.getName().trim().isEmpty() && form.getUri() != null
-                && !form.getUri().trim().isEmpty()
+        if (form.getName() != null && !form.getName().trim().isEmpty()
+                && form.getUri() != null && !form.getUri().trim().isEmpty() 
                 && conceptTypeManager.getByUriAndOwner(form.getUri(), user) == null) {
-            conceptTypeManager.create(form, user);
-        } else if (form.getUri() != null && !form.getUri().trim().isEmpty()
-                && conceptTypeManager.getByUriAndOwner(form.getUri(), user) != null) {
+                conceptTypeManager.create(form, user);
+        } else if(form.getUri() != null && !form.getUri().trim().isEmpty()
+                && conceptTypeManager.getByUriAndOwner(form.getUri(), user) != null){
             model.addAttribute("show_alert", true);
             model.addAttribute("alert_msg", "A concept type with this URI exists.");
             model.addAttribute("alert_type", "danger");
@@ -47,7 +47,7 @@ public class AddConceptTypeController {
             model.addAttribute("conceptTypeForm", form);
             return "auth/concepts/types/add";
         }
-
+        
         return "redirect:/auth/concepts/types/list";
     }
 }
