672798f2fe3d994b75fd59735b83f90521b287b9
Namratha <Namratha@sfi-c02x40qzj1g9.dhcp.asu.edu>
2019-11-21 15:34:04 -0700

    [CITE-38] Add and edit mode in validator.

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/validation/CitationConceptValidator.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/validation/CitationConceptValidator.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import edu.asu.diging.citesphere.core.model.IUser;
+import edu.asu.diging.citesphere.core.model.bib.ICitationConcept;
 import edu.asu.diging.citesphere.core.service.ICitationConceptManager;
 import edu.asu.diging.citesphere.web.forms.CitationConceptForm;
 
@@ -22,19 +23,20 @@ public class CitationConceptValidator implements Validator {
     public void validate(Object target, Errors errors) {
         CitationConceptForm conceptForm = (CitationConceptForm) target;
         IUser user = (IUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (!errors.hasErrors()) {
-            if (conceptForm.getConceptId() == null) {
+        if (!errors.hasErrors() && conceptForm.getUri() != null) {
+            ICitationConcept concept = conceptManager.getByUriAndOwner(conceptForm.getUri(), user);
+            if (conceptForm.getConceptId() != null && concept != null) {
+              //edit mode
                 if (conceptManager.get(conceptForm.getConceptId()) != null
                         && conceptManager.get(conceptForm.getConceptId()).getOwner() != null
-                        && !(conceptManager.get(conceptForm.getConceptId()).getOwner().getUsername()
-                                .equals(user.getUsername()))) {
+                        && concept.getId().equals(conceptForm.getConceptId())) {
                     errors.rejectValue("uri", "uri", "Only the owner can edit a Concept.");
                 } else if (!conceptManager.get(conceptForm.getConceptId()).getUri()
-                                .equals(conceptForm.getUri())
-                        && conceptManager.getByUriAndOwner(conceptForm.getUri(), user) != null) {
+                                .equals(conceptForm.getUri())) {
                     errors.rejectValue("uri", "uri", "Concept with this uri already exists!");
                 }
-            } else if (conceptForm.getConceptId() != null && conceptManager.getByUriAndOwner(conceptForm.getUri(), user) != null) {
+            } else if(concept != null) { 
+                // add mode
                 errors.rejectValue("uri", "uri", "Concept with this uri already exists!");
             }
         }
