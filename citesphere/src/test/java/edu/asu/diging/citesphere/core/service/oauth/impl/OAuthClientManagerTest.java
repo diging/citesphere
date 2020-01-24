6a316ceefa033c3f36b72b19b7dc46955fa43243
radhikanyati94 <radhikanyati94@gmail.com>
2020-01-24 07:10:05 -0700

    [CITE60] - Delete App feature (#67)
    * [CITE-60] Ability to delete registered app
    
    * [CITE-60] Delete App feature
    
    * [CITE-60] Changes in JSP
    
    * [CITE-60] Junits for deleteClient method
    
    * [CITE-60] User Confirmaton to delete app
    
    * [CITE-60] User confirmation modal
    
    * [CITE-60] Code Fix to remove multiple apps
    
    * [CITE-60] Removed Console logs
    
    * [CITE-60] Review Comments Incorporated

---
+++ citesphere/src/test/java/edu/asu/diging/citesphere/core/service/oauth/impl/OAuthClientManagerTest.java
@@ -0,0 +1,45 @@
+package edu.asu.diging.citesphere.core.service.oauth.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import edu.asu.diging.citesphere.core.repository.oauth.OAuthClientRepository;
+
+public class OAuthClientManagerTest {
+    
+    @Mock
+    private OAuthClientRepository clientRepo;
+    
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    
+    private int accessTokenValidity = 3600;
+    
+    @InjectMocks
+    private OAuthClientManager managerToTest = new OAuthClientManager(clientRepo, bCryptPasswordEncoder, accessTokenValidity);
+    
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void test_deleteClient() {
+        String clientId = "clientId";
+        managerToTest.deleteClient(clientId);
+        Mockito.verify(clientRepo).deleteById(clientId);
+    }
+    
+    @Test
+    public void test_deleteClient_nullId() {
+        String clientId = null;
+        managerToTest.deleteClient(clientId);
+        Mockito.verify(clientRepo, Mockito.never()).deleteById(clientId);
+    }
+
+}
