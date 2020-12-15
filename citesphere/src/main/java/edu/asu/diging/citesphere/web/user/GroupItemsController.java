9af5b200c7c283e01f884e00a6ba45500e50eb07
radhikanyati <radhikanyati94@gmail.com>
2020-12-15 09:59:16 -0800

    [CITE-104] Added loggers

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/GroupItemsController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/GroupItemsController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mysql.jdbc.log.Log;
+
 import edu.asu.diging.citesphere.core.exceptions.GroupDoesNotExistException;
 import edu.asu.diging.citesphere.core.exceptions.ZoteroHttpStatusException;
 import edu.asu.diging.citesphere.core.service.ICitationCollectionManager;
@@ -57,14 +59,14 @@ public class GroupItemsController {
             @RequestParam(defaultValue = "title", required = false, value = "sort") String sort,
             @RequestParam(required = false, value = "columns") String[] columns)
             throws GroupDoesNotExistException, ZoteroHttpStatusException {
-        
+        logger.error("*** inside controller ****");
         Integer pageInt = 1;
         try {
             pageInt = new Integer(page);
         } catch (NumberFormatException ex) {
             logger.warn("Trying to access invalid page number: " + page);
         }
-
+        logger.error("*** pageInt ****" + pageInt);
         IUser user = (IUser) authentication.getPrincipal();
         CitationResults results = citationManager.getGroupItems(user, groupId, collectionId, pageInt, sort);
         model.addAttribute("items", results.getCitations());
@@ -78,7 +80,7 @@ public class GroupItemsController {
         model.addAttribute("results", results);
         // more than 200 really don't make sense here, this needs to be changed
         model.addAttribute("citationCollections", collectionManager.getAllCollections(user, groupId, collectionId, "title", 200));
-        
+        logger.error("*** citation results ****" + results);
         List<String> allowedColumns = Arrays.asList(availableColumns.split(","));
         List<String> shownColumns = new ArrayList<>();
         if (columns != null && columns.length > 0) {
@@ -88,14 +90,14 @@ public class GroupItemsController {
                 }
             }
         }
-        
+        logger.error("*** shown columns ****" + shownColumns);
         model.addAttribute("columns", shownColumns);
         model.addAttribute("availableColumns", allowedColumns);
         
         
         ICitationGroup group = groupManager.getGroup(user, groupId);
         List<BreadCrumb> breadCrumbs = new ArrayList<>();
-        
+        logger.error("*** group ****" + group);
         ICitationCollection collection = null;
         if (collectionId != null) {
             collection = collectionManager.getCollection(user, groupId, collectionId);
@@ -114,6 +116,7 @@ public class GroupItemsController {
         breadCrumbs.add(new BreadCrumb(group.getName(), BreadCrumbType.GROUP, group.getGroupId() + "", group));
         Collections.reverse(breadCrumbs);
         model.addAttribute("breadCrumbs", breadCrumbs);
+        logger.error("*** breadCrumbs ****" + breadCrumbs);
         return "auth/group/items";
     }
 }
