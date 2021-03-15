78546ffd041cfffdfb556f2237c6303c95225397
SayaliTanawade <tanawade.sayali@gmail.com>
2021-03-15 12:54:04 -0700

    [CITE-85] Code clean up to remove unused imports

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/DeleteCitationController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/DeleteCitationController.java
@@ -2,8 +2,6 @@ package edu.asu.diging.citesphere.web.user;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.social.zotero.exception.ZoteroConnectionException;
