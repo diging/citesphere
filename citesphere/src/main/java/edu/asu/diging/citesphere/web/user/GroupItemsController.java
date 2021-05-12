ea8d3fc2f071fe511248d2dd0db50a3aebc34a05
Narmada Ravali Namburi <ravali.namburi95@gmail.com>
2021-05-12 07:39:31 -0700

    [CITE-93] removed unnecessary code

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/GroupItemsController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/GroupItemsController.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
