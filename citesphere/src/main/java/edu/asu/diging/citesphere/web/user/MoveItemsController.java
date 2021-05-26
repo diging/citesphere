37c81859148f7e4c89807e0236554f6c8a210454
Narmada Ravali Namburi <ravali.namburi95@gmail.com>
2021-05-26 12:49:07 -0700

    [CITE-86] small change

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/MoveItemsController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/MoveItemsController.java
@@ -87,7 +87,8 @@ public class MoveItemsController {
     @RequestMapping(value = { "/auth/group/{zoteroGroupId}/items/move/{targetCollectionId}/sync/start",
             "/auth/group/{zoteroGroupId}/collection/{parentcollectionId}/items/move/{targetCollectionId}/sync/start" })
     public @ResponseBody Sync startSync(Authentication authentication,
-            @PathVariable("zoteroGroupId") String zoteroGroupId, @PathVariable("targetCollectionId") String collectionId,
+            @PathVariable("zoteroGroupId") String zoteroGroupId,
+            @PathVariable("targetCollectionId") String collectionId,
             @RequestParam(defaultValue = "1", required = false, value = "page") String page) {
         try {
             citationManager.getGroupItems((IUser) authentication.getPrincipal(), zoteroGroupId, collectionId,
@@ -107,21 +108,22 @@ public class MoveItemsController {
     @RequestMapping(value = { "/auth/group/{zoteroGroupId}/items/move/{targetCollectionId}/totalItems",
             "/auth/group/{zoteroGroupId}/collection/{parentcollectionId}/items/move/{targetCollectionId}/totalItems" })
     public @ResponseBody Long getTotalCitationsCollection(Authentication authentication,
-            @PathVariable("zoteroGroupId") String zoteroGroupId, @PathVariable("targetCollectionId") String collectionId) {
+            @PathVariable("zoteroGroupId") String zoteroGroupId,
+            @PathVariable("targetCollectionId") String collectionId) {
         ICitationCollection collection = collectionManager.getCollection((IUser) authentication.getPrincipal(),
                 zoteroGroupId, collectionId);
         return collection.getNumberOfItems();
     }
-}
+    
+    class Sync {
+        String status;
 
-class Sync {
-    String status;
+        public String getStatus() {
+            return status;
+        }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
+        public void setStatus(String status) {
+            this.status = status;
+        }
     }
 }
