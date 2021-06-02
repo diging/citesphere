f59e103a4a0e1bc968f02b3e3889801f2dabf691
Maulik Vajubhai Limbadiya (Student) <mlimbadi@sundevils.asu.edu>
2021-06-02 09:16:08 -0700

    [CITE-136] - Working to add viaf as source for the authority search

--- citesphere/src/main/java/edu/asu/diging/citesphere/core/authority/impl/ViafAuthorityImporter.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/core/authority/impl/ViafAuthorityImporter.java
@@ -1,10 +1,17 @@
 package edu.asu.diging.citesphere.core.authority.impl;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.annotation.PostConstruct;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -20,10 +27,13 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import edu.asu.diging.citesphere.core.authority.IImportedAuthority;
 import edu.asu.diging.citesphere.core.authority.impl.ViafResponse.Data;
 import edu.asu.diging.citesphere.core.exceptions.AuthorityServiceConnectionException;
+import edu.asu.diging.citesphere.model.authority.IAuthorityEntry;
+import edu.asu.diging.citesphere.model.authority.impl.AuthorityEntry;
 import edu.asu.diging.citesphere.web.user.AuthoritySearchResult;
 
 @Component
@@ -34,19 +44,31 @@ public class ViafAuthorityImporter extends BaseAuthorityImporter {
 
     @Value("${_viaf_url_regex}")
     private String viafUrlRegex;
+    
+    @Value("${_viaf_authority_search_keyword}")
+    private String viafSearchKeyword;
+
+    @Value("${_viaf_url}")
+    private String viafURL;
+
+    private RestTemplate restTemplate;
+    
+    @PostConstruct
+    private void postConstruct() {
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        HttpClient httpClient = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
+        factory.setHttpClient(httpClient);
+
+        restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(factory);
+    }
 
     /* (non-Javadoc)
      * @see edu.asu.diging.citesphere.authority.impl.AuthorityImporter#isResponsible(java.lang.String)
      */
     @Override
     public boolean isResponsible(String uri) {
-        Pattern pattern = Pattern.compile(viafUrlRegex);
-        Matcher matcher = pattern.matcher(uri);
-
-        if (matcher.matches()) {
-            return true;
-        }
-
         return false;
     }
 
@@ -94,12 +116,66 @@ public class ViafAuthorityImporter extends BaseAuthorityImporter {
     }
 
     @Override
-    public AuthoritySearchResult searchAuthorities(String firstName, String lastName, int page, int pageSize) {
-        throw new UnsupportedOperationException();
+    public AuthoritySearchResult searchAuthorities(String firstName, String lastName, int page, int pageSize)
+        throws AuthorityServiceConnectionException {
+
+        RequestEntity<Void> request;
+        try {
+            String url = viafURL + viafSearchKeyword + URLEncoder
+                    .encode(this.getviafSearchString(firstName, lastName), StandardCharsets.UTF_8.toString())
+                    + "&page=" + page;
+            URI uri = UriComponentsBuilder.fromUriString(url.toString()).build(true).toUri();
+
+            request = RequestEntity.get(uri).accept(MediaType.APPLICATION_JSON).build();
+        } catch (UnsupportedEncodingException e) {
+            throw new AuthorityServiceConnectionException("Exception occured while creating URI for authority search",
+                    e);
+        }
+        ResponseEntity<ConceptpowerResponse> response = null;
+        try {
+            response = restTemplate.exchange(request, ConceptpowerResponse.class);
+        } catch (RestClientException ex) {
+            throw new AuthorityServiceConnectionException(ex);
+        }
+
+        List<IAuthorityEntry> authorityEntries = new ArrayList<IAuthorityEntry>();
+        AuthoritySearchResult searchResult = new AuthoritySearchResult();
+        if (response.getStatusCode() == HttpStatus.OK) {
+            ConceptpowerResponse conceptEntries = response.getBody();
+            if (conceptEntries.getConceptEntries() != null) {
+                for (ConceptpowerEntry conceptEntry : conceptEntries.getConceptEntries()) {
+                    IAuthorityEntry authority = new AuthorityEntry();
+                    authority.setName(conceptEntry.getLemma());
+                    authority.setUri(conceptEntry.getConcept_uri());
+                    authority.setDescription(conceptEntry.getDescription());
+                    authorityEntries.add(authority);
+                }
+                searchResult.setFoundAuthorities(authorityEntries);
+                searchResult.setTotalPages((int) Math
+                        .ceil(conceptEntries.getPagination().getTotalNumberOfRecords() / new Float(pageSize)));
+            }
+        } else {
+            throw new AuthorityServiceConnectionException(response.getStatusCode().toString());
+        }
+        return searchResult;
     }
 
     @Override
     public boolean isResponsibleForSearch(String source) {
+        if (source.equals(VIAF)) {
+            return true;
+        }
         return false;
     }
+    
+    private String getviafSearchString(String firstName, String lastName) {
+        String viafSearchString = "query=local.personalNames+all+\"";
+        if (firstName != null && lastName != null) {
+            viafSearchString = firstName + "+" + lastName;
+        } else {
+            viafSearchString = firstName == null ? lastName : firstName;
+        }
+        return viafSearchString;
+    }
+    
 }
