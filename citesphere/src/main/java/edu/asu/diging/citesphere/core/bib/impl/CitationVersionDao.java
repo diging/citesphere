7b2d7e4cd19f851320346478df3cc598fe811265
Maulik Vajubhai Limbadiya (Student) <mlimbadi@sundevils.asu.edu>
2021-08-16 16:19:25 -0700

    [CITE-90] - Addressed review comments - refactored the query to fetch the versions page

--- citesphere/src/main/java/edu/asu/diging/citesphere/core/bib/impl/CitationVersionDao.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/core/bib/impl/CitationVersionDao.java
@@ -27,28 +27,33 @@ public class CitationVersionDao implements ICitationVersionsDao {
 
     @Override
     public List<CitationVersion> getVersions(String groupId, String key, int page, int pageSize) {
-        QueryBuilder jqlQuery = QueryBuilder.byInstanceId(key, Citation.class);
-        List<CdoSnapshot> versions = javers.findSnapshots(jqlQuery.build()).stream()
-                .filter(snapshot -> snapshot.getType() != SnapshotType.TERMINAL
-                        && groupId.equals((String) snapshot.getPropertyValue(GROUP_PROPERTY)))
-                .collect(Collectors.toList());
         int offset = page * pageSize;
-        if (offset >= versions.size()) {
-            return new ArrayList<>();
+        List<CitationVersion> result = new ArrayList<>();
+        List<CdoSnapshot> versions = javers.findSnapshots(QueryBuilder.byInstanceId(key, Citation.class)
+                .withSnapshotTypeUpdate().skip(offset).limit(pageSize).build());
+        result.addAll(versions.stream().filter(version -> version.getPropertyValue(GROUP_PROPERTY).equals(groupId))
+                .map(version -> toCitationVersion(version, key)).collect(Collectors.toList()));
+
+        // If the page is still empty and if the index of the last element of the
+        // current page is less than or equal to the total version count, add the initial version
+        if (result.size() < pageSize && getTotalCount(groupId, key) <= offset + pageSize) {
+            CdoSnapshot initialVersion = javers
+                    .findSnapshots(QueryBuilder.byInstanceId(key, Citation.class).withSnapshotType(SnapshotType.INITIAL)
+                            .build())
+                    .stream().filter(version -> version.getPropertyValue(GROUP_PROPERTY).equals(groupId)).findFirst()
+                    .orElseGet(null);
+            if (initialVersion != null) {
+                result.add(toCitationVersion(initialVersion, key));
+            }
         }
-        List<CitationVersion> itemVersions = versions.subList(offset, Math.min(offset + pageSize, versions.size()))
-                .stream().map(snapshot -> toCitationVersion(snapshot, key)).collect(Collectors.toList());
-        return itemVersions;
+        return result;
     }
 
     @Override
     public int getTotalCount(String groupId, String key) {
-        QueryBuilder jqlQuery = QueryBuilder.byInstanceId(key, Citation.class);
-        return javers.findSnapshots(jqlQuery.build()).stream()
-                .filter(snapshot -> snapshot.getType() != SnapshotType.TERMINAL
-                        && groupId.equals((String) snapshot.getPropertyValue(GROUP_PROPERTY)))
-                .collect(Collectors.toList()).size();
-
+        QueryBuilder jqlQuery = QueryBuilder.byInstanceId(key, Citation.class).withSnapshotTypeUpdate();
+        return (int) javers.findSnapshots(jqlQuery.build()).stream()
+                .filter(version -> version.getPropertyValue(GROUP_PROPERTY).equals(groupId)).count() + 1;
     }
 
     @Override
