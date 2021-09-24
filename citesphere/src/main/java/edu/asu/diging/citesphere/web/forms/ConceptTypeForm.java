a0d64a118f58b841a6f8bfc8c6cb18da2d0022b5
Charishma Boddu <charishma0249@gmail.com>
2021-09-24 10:33:57 -0700

    Cite 157 edit concept is broken (#176)
    * Add URI validations in Edit Concept Controller
    
    * Add URI validations in Add Concept Controller
    
    * [CITE-157] Fix codefactor checks
    
    * [CITE-157] Fix codefactor checks
    
    * [CITE-157] Fix codechecks
    
    * [CITE-157] fix codechecks
    
    * [CITE-157] Add URI Validations in Concept Type
    
    * [CITE-157] Fix codechecks validation
    
    * [CITE-157] code refactoring
    
    * [CITE-157] Fix codefactor checks
    
    * [CITE-157] Remove unused imports
    
    * [CITE-157] addressed review comments: added Thymleaf template for errors
    
    * [CITE-156] formatting
    
    * [CITE-157] formatting

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/forms/ConceptTypeForm.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/forms/ConceptTypeForm.java
@@ -2,12 +2,15 @@ package edu.asu.diging.citesphere.web.forms;
 
 import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.URL;
+
 public class ConceptTypeForm {
 
     @NotEmpty(message = "Name cannot be empty.")
     private String name;
     private String description;
     @NotEmpty(message = "Uri cannot be empty.")
+    @URL
     private String uri;
 
     private String conceptTypeId;
