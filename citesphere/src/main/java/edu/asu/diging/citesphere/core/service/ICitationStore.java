7b8d935296fe6835aacd303ece4f1b890274f7f6
Maulik Limbadiya <limbadiyamaulik@gmail.com>
2021-10-15 11:23:32 -0700

    [CITE-125] - bump citesphere-model version

--- citesphere/src/main/java/edu/asu/diging/citesphere/core/service/ICitationStore.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/core/service/ICitationStore.java
@@ -27,6 +27,11 @@ public interface ICitationStore {
      */
     List<ICitation> getAttachments(String id);
     
+    /**
+     * Method to fetch non-deleted notes for an item.
+     * @param id Key of the item.
+     * @return Notes for the provided item key.
+     */
     List<ICitation> getNotes(String id);
 
     void deleteCitationByGroupId(String groupId);
