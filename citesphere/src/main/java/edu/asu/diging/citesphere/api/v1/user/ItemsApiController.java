2fe230eea42c22ba55413609b174339eacb87a3a
Maulik Limbadiya <limbadiyamaulik@gmail.com>
2021-10-22 12:11:39 -0700

    [CITE-41] - restructuring the html elements

--- citesphere/src/main/java/edu/asu/diging/citesphere/api/v1/user/ItemsApiController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/api/v1/user/ItemsApiController.java
@@ -2,7 +2,6 @@ package edu.asu.diging.citesphere.api.v1.user;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.ArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
