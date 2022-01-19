890f3dead60810292fd9b66122fa304b520e510f
Maulik Limbadiya <limbadiyamaulik@gmail.com>
2022-01-19 13:45:41 -0700

    [CITE-91] - Fix todo comment format

--- citesphere/src/main/java/edu/asu/diging/citesphere/core/exceptions/CannotFindCitationVersionException.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/core/exceptions/CannotFindCitationVersionException.java
@@ -9,28 +9,28 @@ public class CannotFindCitationVersionException extends Exception {
 
     public CannotFindCitationVersionException() {
         super();
-        // TODO Auto-generated constructor stub
+        // TODO: Auto-generated constructor stub
     }
 
     public CannotFindCitationVersionException(String message, Throwable cause, boolean enableSuppression,
             boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
-        // TODO Auto-generated constructor stub
+        // TODO: Auto-generated constructor stub
     }
 
     public CannotFindCitationVersionException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
+        // TODO: Auto-generated constructor stub
     }
 
     public CannotFindCitationVersionException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
+        // TODO: Auto-generated constructor stub
     }
 
     public CannotFindCitationVersionException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
+        // TODO: Auto-generated constructor stub
     }
 
 }
