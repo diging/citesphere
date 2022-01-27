713ce4d64c65888ce2faaba7f7d7018046062673
Charishma0249 <charishma0249@gmail.com>
2022-01-27 08:49:55 -0700

    [CITE-132] Addressed review comments

--- citesphere/src/main/java/edu/asu/diging/citesphere/core/service/IAsyncCitationProcessor.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/core/service/IAsyncCitationProcessor.java
@@ -1,10 +1,12 @@
 package edu.asu.diging.citesphere.core.service;
 
+import java.util.concurrent.Future;
+
 import edu.asu.diging.citesphere.user.IUser;
 
 public interface IAsyncCitationProcessor {
 
-    void sync(IUser user, String groupId, long contentVersion, String collectionId);
+    Future<String> sync(IUser user, String groupId, long contentVersion, String collectionId);
 
     
 }
