1fdc99cc627155d9ed330eaefe3da469206ede92
Maulik Limbadiya <limbadiyamaulik@gmail.com>
2022-02-22 10:06:00 -0700

    Story/cite 136 (#177)
    * [CITE-137] - Implemented add new authority functionality with manual authority creation and through search.
    
    * [CITE-137] Exclude already imported authorities on search by URI
    
    * [CITE-137] Removed non-applicable elements from the search
    
    * [CITE-137] - Removed non-applicable js component. Replaced local URIs with placeholders for authority API and prefix.
    
    * [CITE-137] placeholder correction.
    
    * [CITE-137] - Added test case
    
    * [CITE-136] - Working to add viaf as source for the authority search
    
    * [CITE-137] - Addressed review comments - refactored API endpoints, moved URI generation to service class
    
    * [CITE-136] - Enabled search with viaf as source. Added VIAF tab to search pop-up on edit citation page.
    
    * [CITE-136] Added VIAF search tab for managed authorities tab
    
    * [CITE-136] - Reverted some previous changes. Added boolean parameter for searching authorities by URI indicating if to search in importers.
    
    * [CITE-136] - disabled authority import through edit item page. Fixed bug for multiple item delete failure for authorities list.
    
    * [CITE-136] - Fixed pagination for viaf search tab, fixed indentations, code cleanup
    
    * [CITE-136] - Added javadocs
    
    * [CITE-136] - minor correction
    
    * [CITE-136] - addressed review comments - adding comments and javadocs for better understanding
    
    * [CITE-136] - fixed bugs - display group name, pagination issue for viaf search tab
    
    * [CITE-136] - fixed pagination bug for viaf search tab for create page
    
    * [CITE-136] - addressed review comments - edited javadocs, removed unnecessary code block
    
    * [CITE-136] - minor correction
    
    * [CITE-136] - fixed editing an already existing entry for authors, editors
    
    Co-authored-by: Maulik Vajubhai Limbadiya (Student) <mlimbadi@sundevils.asu.edu>

---
+++ citesphere/src/main/java/edu/asu/diging/citesphere/core/authority/impl/ViafSearchResponseWrapper.java
@@ -0,0 +1,70 @@
+package edu.asu.diging.citesphere.core.authority.impl;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Wrapper object for VIAF authority search response
+ * @author Maulik Limbadiya
+ *
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ViafSearchResponseWrapper {
+
+    private ViafSearchResponse searchRetrieveResponse;
+
+    public ViafSearchResponse getSearchRetrieveResponse() {
+        return searchRetrieveResponse;
+    }
+
+    public void setSearchRetrieveResponse(ViafSearchResponse searchRetrieveResponse) {
+        this.searchRetrieveResponse = searchRetrieveResponse;
+    }
+    
+    public static class ViafSearchResponse {
+        private int numberOfRecords;
+        private List<ViafSearchItem> records;
+
+        public int getNumberOfRecords() {
+            return numberOfRecords;
+        }
+
+        public void setNumberOfRecords(int numberOfRecords) {
+            this.numberOfRecords = numberOfRecords;
+        }
+
+        public List<ViafSearchItem> getRecords() {
+            return records;
+        }
+
+        public void setRecords(List<ViafSearchItem> records) {
+            this.records = records;
+        }
+    }
+    
+    public static class ViafSearchItem {
+        private ViafSearchRecord record;
+
+        public ViafSearchRecord getRecord() {
+            return record;
+        }
+
+        public void setRecord(ViafSearchRecord record) {
+            this.record = record;
+        }
+    }
+    
+    public static class ViafSearchRecord {
+        private ViafRecordData recordData;
+
+        public ViafRecordData getRecordData() {
+            return recordData;
+        }
+
+        public void setRecordData(ViafRecordData recordData) {
+            this.recordData = recordData;
+        }
+    }
+    
+}
