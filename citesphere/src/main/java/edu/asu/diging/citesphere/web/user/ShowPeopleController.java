1ad2713a92555ca4f8240b28e2ca821e177b4966
Pratik Prakash Giri <32401726+PratikGiri@users.noreply.github.com>
2022-03-24 11:48:22 -0700

    [CITE-171] implemented person index page

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/ShowPeopleController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/ShowPeopleController.java
@@ -5,47 +5,29 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.social.zotero.exception.ZoteroConnectionException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.asu.diging.citesphere.core.exceptions.GroupDoesNotExistException;
 import edu.asu.diging.citesphere.core.exceptions.ZoteroHttpStatusException;
-import edu.asu.diging.citesphere.core.exceptions.ZoteroItemCreationFailedException;
-import edu.asu.diging.citesphere.core.service.ICitationCollectionManager;
-import edu.asu.diging.citesphere.core.service.ICitationConceptManager;
 import edu.asu.diging.citesphere.core.service.ICitationManager;
-import edu.asu.diging.citesphere.core.service.IConceptTypeManager;
-import edu.asu.diging.citesphere.core.user.IUserManager;
-import edu.asu.diging.citesphere.core.util.model.ICitationHelper;
 import edu.asu.diging.citesphere.model.bib.ICitation;
-import edu.asu.diging.citesphere.model.bib.ICreator;
 import edu.asu.diging.citesphere.model.bib.IPerson;
-import edu.asu.diging.citesphere.model.bib.ItemType;
-import edu.asu.diging.citesphere.model.bib.impl.Citation;
 import edu.asu.diging.citesphere.user.IUser;
-import edu.asu.diging.citesphere.web.forms.CitationForm;
 import edu.asu.diging.citesphere.model.bib.impl.CitationResults;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.AbstractMap.SimpleEntry;
-import static java.util.stream.Collectors.toMap;
 
 @Controller
 public class ShowPeopleController {
@@ -54,8 +36,10 @@ public class ShowPeopleController {
 
     @Autowired
     private ICitationManager citationManager;
+    
+    Map<IPerson, List<ICitation>> iPersoniCitSortedMap = new TreeMap<IPerson, List<ICitation>>();
 
-    @RequestMapping(value = {"/auth/group/{zoteroGroupId}/items/showPeople", "/auth/group/{zoteroGroupId}/items/showCitation/{index}"})
+    @RequestMapping(value = "/auth/group/{zoteroGroupId}/items/showPeople")
     public String showPeople(Model model, Authentication authentication, @PathVariable("zoteroGroupId") String groupId,
             @PathVariable(value="index", required=false) String index, @PathVariable(value="collectionId", required=false) String collectionId,
             @RequestParam(defaultValue = "1", required = false, value = "page") String page,
@@ -83,12 +67,13 @@ public class ShowPeopleController {
         model.addAttribute("currentPage", pageInt);
         model.addAttribute("sort", sort);
         
-        Map<IPerson, List<ICitation>> personCitationMap=new HashMap<IPerson, List<ICitation>>();  
+        Map<IPerson, List<ICitation>> personCitationMap=new HashMap<IPerson, List<ICitation>>();
         
         results.getCitations().stream().forEach(
                 iCitation -> (Stream.of(iCitation.getAuthors().stream(), iCitation.getEditors().stream(), 
                         iCitation.getOtherCreators().stream().map(iOtherCreator -> iOtherCreator.getPerson()))
-                        .reduce(Stream::concat).orElseGet(Stream::empty)).sorted().forEach(
+                        .reduce(Stream::concat).orElseGet(Stream::empty)).
+                filter(distinctByKey(p -> p.getFirstName())).forEach(
                         au -> {
                             boolean containKey = false;
                                 for (IPerson key : personCitationMap.keySet()) {
@@ -108,19 +93,51 @@ public class ShowPeopleController {
                             }
                         ));
         
-        Collection<IPerson> unsorted = personCitationMap.keySet();
-        List<IPerson> peopleList = new ArrayList<IPerson>(unsorted);  
-        
-        peopleList = peopleList.stream().sorted().collect(Collectors.toList());
+        Map<IPerson, List<ICitation>> personCitationTMap = new TreeMap<IPerson, List<ICitation>>(personCitationMap);
+
+        Collection<IPerson> keyCollection = personCitationTMap.keySet();
+        List<IPerson> peopleList = new ArrayList<IPerson>(keyCollection);
         
         model.addAttribute("people", peopleList);
+        
+        iPersoniCitSortedMap = new TreeMap<IPerson, List<ICitation>>(personCitationTMap);
 
-    if (index != null && !index.isEmpty()) {
-        model.addAttribute("involvedCitations", personCitationMap.get(personCitationMap.keySet().toArray()[Integer.parseInt(index)]));
+        if (index != null && !index.isEmpty()) {
+            model.addAttribute("involvedCitations", personCitationTMap.get(personCitationTMap.keySet().toArray()[Integer.parseInt(index)]));
+            return "auth/group/showCitations";
+        }
+        else
+            return "auth/group/showPeople";
+        }
+    
+    @RequestMapping(value = "/auth/group/{zoteroGroupId}/items/showCitation/{index}")
+    public String showCitation(Model model, Authentication authentication, @PathVariable("zoteroGroupId") String groupId,
+            @PathVariable(value="index", required=false) String index, @PathVariable(value="collectionId", required=false) String collectionId,
+            @RequestParam(defaultValue = "1", required = false, value = "page") String page,
+            @RequestParam(defaultValue = "title", required = false, value = "sort") String sort,
+            @RequestParam(required = false, value = "columns") String[] columns) {
+        
+        Integer pageInt = 1;
+        try {
+            pageInt = new Integer(page);
+        } catch (NumberFormatException ex) {
+            logger.warn("Trying to access invalid page number: " + page);
+        }
+        model.addAttribute("collectionId", collectionId);
+        model.addAttribute("zoteroGroupId", groupId);
+        model.addAttribute("currentPage", pageInt);
+        model.addAttribute("sort", sort);
+        
+        if (index != null && !index.isEmpty()) {
+            model.addAttribute("involvedCitations", iPersoniCitSortedMap.get(iPersoniCitSortedMap.keySet().toArray()[Integer.parseInt(index)]));
+        }
         return "auth/group/showCitations";
     }
-    else
-        return "auth/group/showPeople";
-    }
-
+    
+    public static <T> Predicate<T> distinctByKey(
+            Function<? super T, ?> keyExtractor) {
+          
+            Map<Object, Boolean> seen = new ConcurrentHashMap<>(); 
+            return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null; 
+        }
 }
