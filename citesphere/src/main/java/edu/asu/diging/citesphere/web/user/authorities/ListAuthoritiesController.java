344ec063b1fee2d88d9ef6a65be89181d29d1966
Trilekha Mylavarapu <trills@Trilekhas-MacBook-Air.local>
2022-03-29 11:13:43 -0700

    [CITE-172] list all citations that reference that authority

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/authorities/ListAuthoritiesController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/authorities/ListAuthoritiesController.java
@@ -41,40 +41,40 @@ public class ListAuthoritiesController {
         List<ICitationGroup> groups = citationManager.getGroups(user);
         List<String> groupIds = new ArrayList<String>();
         for (int i = 0; i < groups.size(); i++) {
-        	groupIds.add(String.valueOf(groups.get(i).getGroupId()));
+            groupIds.add(String.valueOf(groups.get(i).getGroupId()));
         }
         CitationResults results = null;
         Set<ICitation> citations = new HashSet<ICitation>();
         for (String groupId : groupIds) {
-        	results = citationManager.getGroupItems(user, groupId, null, 1, "title");
-        	for (ICitation citation : results.getCitations()) {
-        		String name = entry.getName();
-        		Set<IPerson> authors = citation.getAuthors();
-        		for (IPerson person : authors) {
-        			if (person.getName().equals(" " + name)) {
-        				citations.add(citation);
-        				break;
-        			}
-        		}
-        		Set<IPerson> editors = citation.getEditors();
-        		for (IPerson person : editors) {
-        			if (person.getName().equals(" " + name) && !citations.contains(citation)) {
-        				citations.add(citation);
-        				break;
-        			}
-        		}
-        		Set<ICreator> otherCreators = citation.getOtherCreators();
-        		Set<IPerson> creators = new HashSet<IPerson>();
-        		for (ICreator creator : otherCreators) {
-        			creators.add(creator.getPerson());
-        		}
-        		for (IPerson person : creators) {
-        			if (person.getName().equals(" " + name) && !citations.contains(citation)) {
-        				citations.add(citation);
-        				break;
-        			}
-        		}
-        	}
+            results = citationManager.getGroupItems(user, groupId, null, 1, "title");
+            for (ICitation citation : results.getCitations()) {
+                String name = entry.getName();
+                Set<IPerson> authors = citation.getAuthors();
+                for (IPerson person : authors) {
+                    if (person.getName().equals(" " + name)) {
+                        citations.add(citation);
+                        break;
+                    }
+                }
+                Set<IPerson> editors = citation.getEditors();
+                for (IPerson person : editors) {
+                    if (person.getName().equals(" " + name) && !citations.contains(citation)) {
+                        citations.add(citation);
+                        break;
+                    }
+                }
+                Set<ICreator> otherCreators = citation.getOtherCreators();
+                Set<IPerson> creators = new HashSet<IPerson>();
+                for (ICreator creator : otherCreators) {
+                    creators.add(creator.getPerson());
+                }
+                for (IPerson person : creators) {
+                    if (person.getName().equals(" " + name) && !citations.contains(citation)) {
+                        citations.add(citation);
+                        break;
+                    }
+                }
+            }
         }
         model.addAttribute("items", citations);
         model.addAttribute("name", entry.getName());
