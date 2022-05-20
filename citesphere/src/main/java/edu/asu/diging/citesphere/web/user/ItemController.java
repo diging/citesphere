97811496005c2cf98191a5ca551c925fbe403484
Pratik Prakash Giri <32401726+PratikGiri@users.noreply.github.com>
2022-05-20 16:46:41 -0700

    Implementing delete functionality initial Commit

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/ItemController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/ItemController.java
@@ -2,24 +2,31 @@ package edu.asu.diging.citesphere.web.user;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.social.zotero.exception.ZoteroConnectionException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.asu.diging.citesphere.core.exceptions.CannotFindCitationException;
+import edu.asu.diging.citesphere.core.exceptions.CitationIsOutdatedException;
 import edu.asu.diging.citesphere.core.exceptions.GroupDoesNotExistException;
 import edu.asu.diging.citesphere.core.exceptions.ZoteroHttpStatusException;
+import edu.asu.diging.citesphere.core.exceptions.ZoteroItemCreationFailedException;
 import edu.asu.diging.citesphere.core.search.service.SearchEngine;
 import edu.asu.diging.citesphere.core.service.ICitationManager;
 import edu.asu.diging.citesphere.core.service.impl.CitationPage;
+import edu.asu.diging.citesphere.core.service.impl.async.AsyncDeleteCitationsResponse;
 import edu.asu.diging.citesphere.model.bib.ICitation;
 import edu.asu.diging.citesphere.user.IUser;
+import edu.asu.diging.citesphere.model.bib.IGilesUpload;
 
 @Controller
 public class ItemController {
@@ -52,6 +59,7 @@ public class ItemController {
             List<ICitation> attachments = citationManager.getAttachments((IUser)authentication.getPrincipal(), zoteroGroupId, itemId);
             model.addAttribute("attachments", attachments);
             model.addAttribute("citation", citation);
+            model.addAttribute("itemId", itemId);
             List<String> fields = new ArrayList<>();
             citationManager.getItemTypeFields((IUser)authentication.getPrincipal(), citation.getItemType()).forEach(f -> fields.add(f.getFilename()));
             model.addAttribute("fields", fields);
@@ -63,5 +71,27 @@ public class ItemController {
             model.addAttribute("sortBy", sortBy);
         }
         return "auth/group/item";
+    }
+
+    // change here: 1. return type of fn: @ResponseBody AsyncDeleteCitationsResponse
+    @RequestMapping(value = "/auth/group/{zoteroGroupId}/file/delete", method = RequestMethod.POST)
+    public @ResponseBody AsyncDeleteCitationsResponse delete(Authentication authentication,
+            @PathVariable("zoteroGroupId") String zoteroGroupId,
+            @RequestParam(value = "fileToDelete", required = false) String fileToDelete,
+           @RequestParam(value = "itemId", required = false) String itemId)
+               throws GroupDoesNotExistException, CannotFindCitationException, ZoteroHttpStatusException, ZoteroConnectionException, CitationIsOutdatedException, ZoteroItemCreationFailedException
+    {
+        // asyncCitationManager.deleteCitations((IUser) authentication.getPrincipal(), zoteroGroupId, citationList);
+//        citation.getGilesUploads().remove(fileToDelete);
+        ICitation citation = citationManager.getCitation((IUser)authentication.getPrincipal(), zoteroGroupId, itemId);
+//        citation.getGilesUploads().remove(fileToDelete);
+        for (Iterator<IGilesUpload> gileUpload = citation.getGilesUploads().iterator(); gileUpload.hasNext(); ) {
+            IGilesUpload g = gileUpload.next();
+            if (g.getDocumentId() != null && g.getDocumentId().equals(fileToDelete))
+                gileUpload.remove();
+        }
+        citationManager.
+        updateCitation((IUser)authentication.getPrincipal(), zoteroGroupId, citation);
+        return new AsyncDeleteCitationsResponse();
     }
 }
