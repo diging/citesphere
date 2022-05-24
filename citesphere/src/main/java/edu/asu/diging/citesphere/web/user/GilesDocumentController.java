afcd193883aea117d56042993da97d37dd817ecb
Charishma0249 <charishma0249@gmail.com>
2022-05-24 17:40:27 -0400

    [CITE-162] Addressed review comments - Used streams to check the fileId

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/GilesDocumentController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/GilesDocumentController.java
@@ -2,8 +2,8 @@ package edu.asu.diging.citesphere.web.user;
 
 import java.awt.TrayIcon.MessageType;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
@@ -18,13 +18,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.common.base.Supplier;
+
+import java.util.stream.Stream;
+
 import edu.asu.diging.citesphere.core.service.ICitationManager;
 import edu.asu.diging.citesphere.core.service.giles.IGilesConnector;
 import edu.asu.diging.citesphere.model.bib.GilesStatus;
 import edu.asu.diging.citesphere.model.bib.ICitation;
 import edu.asu.diging.citesphere.model.bib.IGilesUpload;
-import edu.asu.diging.citesphere.model.bib.impl.GilesFile;
-import edu.asu.diging.citesphere.model.bib.impl.GilesPage;
+import edu.asu.diging.citesphere.model.bib.impl.IGilesFile;
 import edu.asu.diging.citesphere.user.IUser;
 
 @Controller
@@ -37,21 +40,27 @@ public class GilesDocumentController {
     
     @Autowired
     private ICitationManager citationManager;
-
-    private String fileName;
-    
-    private String contentType;
     
     @RequestMapping(value="/auth/group/{zoteroGroupId}/items/{itemId}/giles/{fileId}")
     public void get(HttpServletResponse response, @PathVariable String itemId, @PathVariable String fileId, Authentication authentication) {
         
-        contentType = null;
-        fileName = null;
+        String contentType = null;
+        String fileName = null;
         ICitation citation = citationManager.getCitation(itemId);
         List<IGilesUpload> uploadOptionalList = citation.getGilesUploads().stream().filter(u -> u.getUploadedFile() != null && u.getDocumentStatus().equals(GilesStatus.COMPLETE)).collect(Collectors.toList());
-
+        
         if(uploadOptionalList.size()!=0) {
-            setFileNameAndContentType(uploadOptionalList, fileId);
+            Stream<Object> gilesFilesStream = generateStream(uploadOptionalList);
+            Object[] gilesFilesArray = gilesFilesStream.limit(1).toArray();
+            List<IGilesFile> gilesFiles = (List<IGilesFile>) gilesFilesArray[0];
+                
+            for(IGilesFile gilesFile : gilesFiles) {
+               if(gilesFile != null && gilesFile.getId().equals(fileId)) {
+                  contentType = gilesFile.getContentType();
+                  fileName = gilesFile.getFilename();
+                  break;
+                }
+            }
         }
         else if(uploadOptionalList.size()==0 || contentType==null){
             response.setStatus(org.apache.http.HttpStatus.SC_NOT_FOUND);
@@ -74,71 +83,32 @@ public class GilesDocumentController {
 
     }
     
-    public void setFileNameAndContentType(List<IGilesUpload> uploadOptionalList, String fileId) {
+    private static Stream<Object> generateStream(List<IGilesUpload> uploadOptionalList) {
         
-        Optional<IGilesUpload> gilesUpload = uploadOptionalList.stream().filter(g -> g.getUploadedFile().getId().equals(fileId)).findFirst();
-        
-        if(!gilesUpload.isPresent()) {
-            for(IGilesUpload gilesUploadedFile : uploadOptionalList) {
-                
-                if(gilesUploadedFile.getExtractedText().getId().equals(fileId)) {
-                    contentType = gilesUploadedFile.getExtractedText().getContentType();
-                    fileName = gilesUploadedFile.getExtractedText().getFilename();
-                    break;
-                }
-                
-                List<GilesPage> gilesPages = gilesUploadedFile.getPages();
-                Optional<GilesPage> gilesPageOptional = gilesPages.stream().filter(p -> p.getImage().getId().equals(fileId)
-                        || p.getOcr().getId().equals(fileId)
-                        || p.getText().getId().equals(fileId)).findFirst();
-                
-                if(gilesPageOptional.isPresent()) {
-                    GilesPage targetGilesPage = gilesPageOptional.get();
-                    if(targetGilesPage.getImage().getId().equals(fileId)) {
-                        contentType = targetGilesPage.getImage().getContentType();
-                        fileName = targetGilesPage.getImage().getFilename();
-                    }
-                    if(targetGilesPage.getOcr().getId().equals(fileId)) {
-                        contentType = targetGilesPage.getOcr().getContentType();
-                        fileName = targetGilesPage.getOcr().getFilename();
-                    }
-                    if(targetGilesPage.getText().getId().equals(fileId)) {
-                        contentType = targetGilesPage.getText().getContentType();
-                        fileName = targetGilesPage.getText().getFilename();
-                    }
-                }
-                else {
-                    for(GilesPage gilesPage : gilesPages) {
-                        List<GilesFile> pageAdditionalFiles = gilesPage.getAdditionalFiles();
-                        Optional<GilesFile> pageAdditionalFile = pageAdditionalFiles.stream().filter(a -> a.getId().equals(fileId)).findFirst();
-                        if(pageAdditionalFile.isPresent()) {
-                            contentType = pageAdditionalFile.get().getContentType();
-                            fileName = pageAdditionalFile.get().getFilename();
-                            break;
-                        }
-                    }
+        Stream<Object> gilesUpload = Stream.generate(new Supplier<Object>() {
+            
+            @Override
+            public List<IGilesFile> get() {
+                List<IGilesFile> gilesFiles = new ArrayList<>();
+                for(IGilesUpload gilesUpload : uploadOptionalList) {
                     
-                }
-        
-                if(contentType.isEmpty() || contentType==null) {
-                    List<GilesFile> additionalFiles = gilesUploadedFile.getAdditionaFiles();
-                    Optional<GilesFile> gilesAdditionalFile = additionalFiles.stream().filter(a -> a.getId().equals(fileId)).findFirst();
-              
-                    if(gilesAdditionalFile.isPresent()) {
-                        contentType = gilesAdditionalFile.get().getContentType();
-                        fileName = gilesAdditionalFile.get().getFilename();
-                        break;
+                    gilesFiles.add(gilesUpload.getUploadedFile());
+                    gilesFiles.add(gilesUpload.getExtractedText());
+                    if(gilesUpload.getPages() != null) {
+                        gilesUpload.getPages().forEach(p -> {
+                           gilesFiles.add(p.getImage());
+                           gilesFiles.add(p.getOcr());
+                           gilesFiles.add(p.getText());
+                           p.getAdditionalFiles().forEach(a -> gilesFiles.add(a));
+                       });
+                    }
+                    if(gilesUpload.getAdditionaFiles() != null) {
+                       gilesUpload.getAdditionaFiles().forEach(a -> gilesFiles.add(a));
                     }
                 }
-                
-                    
+                return gilesFiles;
             }
-               
-        }
-        else {
-            contentType = gilesUpload.get().getUploadedFile().getContentType();
-            fileName = gilesUpload.get().getUploadedFile().getFilename();
-        }
-        
+        });
+        return gilesUpload;
     }
 }
