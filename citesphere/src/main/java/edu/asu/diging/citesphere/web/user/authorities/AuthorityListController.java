b749970a7726eb65617d5d8f6d216c0003b423ad
Maulik Limbadiya <limbadiyamaulik@gmail.com>
2022-06-09 09:12:13 -0700

    [CITE-159] - Fetching all authorities the user has access to for the … (#187)
    * [CITE-159] - Fetching all authorities the user has access to for the 'all' option
    
    * [CITE-159] - upgrade citesphere-model version
    
    * [CITE-159] - upgrade citesphere model version
    
    * [CITE-159] - citesphere model version change

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/authorities/AuthorityListController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/authorities/AuthorityListController.java
@@ -1,6 +1,7 @@
 package edu.asu.diging.citesphere.web.user.authorities;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -8,9 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import edu.asu.diging.citesphere.core.service.IAuthorityService;
 import edu.asu.diging.citesphere.core.service.ICitationManager;
-import edu.asu.diging.citesphere.model.authority.IAuthorityEntry;
+import edu.asu.diging.citesphere.model.bib.ICitationGroup;
 import edu.asu.diging.citesphere.user.IUser;
 
 @Controller
@@ -25,8 +27,10 @@ public class AuthorityListController {
     @RequestMapping("/auth/authority/list")
     public String list(Model model, Authentication authentication) {
         IUser user = (IUser)authentication.getPrincipal();
-        model.addAttribute("authorities", authorityService.getAll(user));
-        model.addAttribute("groups", citationManager.getGroups((IUser)authentication.getPrincipal()));
+        List<ICitationGroup> userGroups = citationManager.getGroups((IUser) authentication.getPrincipal());
+        model.addAttribute("authorities", authorityService.getAll(user,
+                userGroups.stream().map(group -> group.getGroupId()).collect(Collectors.toList())));
+        model.addAttribute("groups", userGroups);
         model.addAttribute("displayBy", "all");
         model.addAttribute("username", user.getUsername());
         return "auth/authorities/list";
