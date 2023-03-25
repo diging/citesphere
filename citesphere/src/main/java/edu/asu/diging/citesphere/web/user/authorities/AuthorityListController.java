d0b2498167cb090d96ec43dbd7da314dfcaf2ff2
vardhan-saniko <vrsaniko@asu.edu>
2023-03-24 17:05:30 -0700

    [CITE-185] Sending source list to frontend and written an api to filter based on source

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/authorities/AuthorityListController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/authorities/AuthorityListController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.asu.diging.citesphere.core.service.IAuthorityService;
 import edu.asu.diging.citesphere.core.service.ICitationManager;
+import edu.asu.diging.citesphere.model.authority.IAuthorityEntry;
 import edu.asu.diging.citesphere.model.bib.ICitationGroup;
 import edu.asu.diging.citesphere.user.IUser;
 
@@ -28,9 +29,10 @@ public class AuthorityListController {
     public String list(Model model, Authentication authentication) {
         IUser user = (IUser)authentication.getPrincipal();
         List<ICitationGroup> userGroups = citationManager.getGroups((IUser) authentication.getPrincipal());
-        model.addAttribute("authorities", authorityService.getAll(user,
-                userGroups.stream().map(group -> group.getGroupId()).collect(Collectors.toList())));
+        List<IAuthorityEntry> allAuthorities = authorityService.getAll(user, userGroups.stream().map(group -> group.getGroupId()).collect(Collectors.toList()));
+        model.addAttribute("authorities", allAuthorities);
         model.addAttribute("groups", userGroups);
+        model.addAttribute("importerAuthoritySourceList", allAuthorities.stream().map(authorityEntry -> authorityEntry.source).collect(Collectors.toList()));
         model.addAttribute("displayBy", "all");
         model.addAttribute("username", user.getUsername());
         return "auth/authorities/list";
@@ -53,6 +55,17 @@ public class AuthorityListController {
         model.addAttribute("authorities", authorityService.getUserSpecificAuthorities(user));   
         model.addAttribute("groups", citationManager.getGroups((IUser)authentication.getPrincipal()));
         model.addAttribute("displayBy", "userSpecific");
+        model.addAttribute("username", user.getUsername());
+        return "auth/authorities/list";
+    }
+
+    @RequestMapping("/auth/authority/{source}/list")
+    public String getAuthoritiesForSource(Model model, Authentication authentication,
+            @PathVariable("zoteroGroupId") String source) {
+        IUser user = (IUser)authentication.getPrincipal();
+        model.addAttribute("authorities", authorityService.getUserSpecificAuthorities(user));
+        model.addAttribute("groups", citationManager.getGroups((IUser)authentication.getPrincipal()));
+        model.addAttribute("displayBy", source);
         model.addAttribute("username", user.getUsername());
         return "auth/authorities/list";
     }
