71bffb791b8855de222a9d9e8c3162fb073664a2
aditya <agoyal61@asu.edu>
2023-04-14 16:23:41 -0700

    [CITE-204] Added api for search term in ES

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/SearchController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/SearchController.java
@@ -10,6 +10,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import edu.asu.diging.citesphere.core.exceptions.AuthorityImporterNotFoundException;
+import edu.asu.diging.citesphere.core.exceptions.AuthorityServiceConnectionException;
 import edu.asu.diging.citesphere.core.search.service.SearchEngine;
 import edu.asu.diging.citesphere.core.search.service.impl.ResultPage;
 import edu.asu.diging.citesphere.core.service.IGroupManager;
@@ -100,5 +104,29 @@ public class SearchController {
         Collections.reverse(breadCrumbs);
         model.addAttribute("breadCrumbs", breadCrumbs);
         return "auth/group/items";
+    }
+    
+    @RequestMapping(value = {"/auth/group/{zoteroGroupId}/find/references"})
+    public ResponseEntity<ResultPage> getReferences(Authentication authentication,
+            @PathVariable String zoteroGroupId,
+            @RequestParam(value = "searchTerm", required = false) String searchTerm,
+            @RequestParam(defaultValue = "0", required = false, value = "page") int page,
+            @RequestParam(defaultValue = "20", required = false, value = "pageSize") int pageSize ) {
+    
+        IUser user = (IUser) authentication.getPrincipal();
+        ICitationGroup group = groupManager.getGroup(user, zoteroGroupId);
+
+        Integer pageInt = 1;
+        try {
+            pageInt = new Integer(page);
+        } catch (NumberFormatException ex) {
+            logger.warn("Trying to access invalid page number: " + page);
+        }
+        
+        pageInt = pageInt > 0 ? pageInt : 1;
+
+        ResultPage citations = engine.search(searchTerm, zoteroGroupId, pageInt-1, 50);
+        
+        return new ResponseEntity<ResultPage>(citations, HttpStatus.OK);
     }
 }
