25f252f8f5b916c295736b0cc01201dc3d312f14
vardhan-saniko <vrsaniko@asu.edu>
2023-05-08 17:10:09 -0700

    [CITE-184] added javadoc

--- citesphere/src/main/java/edu/asu/diging/citesphere/core/authority/IImportedAuthority.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/core/authority/IImportedAuthority.java
@@ -8,6 +8,11 @@ public interface IImportedAuthority {
 
     void setName(String name);
 
+    /**
+     * Returns the source of the imported authority.
+     * Ex: Viaf, Conceptpower etc.,
+     * @return a String representing the source of the imported authority.
+     */
     String getSource();
 
     void setSource(String source);
