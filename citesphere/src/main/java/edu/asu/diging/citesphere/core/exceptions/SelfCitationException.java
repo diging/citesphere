be6064e85c4525e78b1d32118118a84b260a3948
PradnyaC11 <pchaud21@asu.edu>
2024-06-14 16:27:06 -0700

    [CITE-204] Resolving issues

--- citesphere/src/main/java/edu/asu/diging/citesphere/core/exceptions/SelfCitationException.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/core/exceptions/SelfCitationException.java
@@ -9,27 +9,27 @@ public class SelfCitationException extends Exception {
 
     public SelfCitationException() {
         super();
-        // TODO Auto-generated constructor stub
+        // Auto-generated constructor stub
     }
 
     public SelfCitationException(String message, Throwable cause, boolean enableSuppression,
             boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
-        // TODO Auto-generated constructor stub
+        // Auto-generated constructor stub
     }
 
     public SelfCitationException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
+        // Auto-generated constructor stub
     }
 
     public SelfCitationException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
+        // Auto-generated constructor stub
     }
 
     public SelfCitationException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
+        // Auto-generated constructor stub
     }
 }
