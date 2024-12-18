d18c6f1bfa7636b65f689b8ea886dcb713557efe
PradnyaC11 <pchaud21@asu.edu>
2024-12-17 17:03:37 -0700

    [CITE-235] Added sync icon to item HTML page and it's endpoint to controller

--- citesphere/src/main/java/edu/asu/diging/citesphere/web/user/SyncItemController.java
+++ citesphere/src/main/java/edu/asu/diging/citesphere/web/user/SyncItemController.java
@@ -29,7 +29,19 @@ public class SyncItemController {
             @RequestParam(defaultValue = "title", required = false, value = "sortBy") String sortBy) throws GroupDoesNotExistException, CannotFindCitationException, ZoteroHttpStatusException {
         citationManager.updateCitationFromZotero((IUser)authentication.getPrincipal(), zoteroGroupId, itemId);
         uploadChecker.add(itemId);
-        uploadChecker.checkUploads();
+        uploadChecker.checkUploads((IUser)authentication.getPrincipal(), zoteroGroupId);
+        return "redirect:/auth/group/{zoteroGroupId}/items/{itemId}?index=" + index +"&page="+page +"&sortBy="+sortBy +"&collectionId="+collectionId;
+    }
+    
+    @RequestMapping(value="/auth/group/{zoteroGroupId}/items/{itemId}/sync/giles") 
+    public String getGilesstatus(Authentication authentication, Model model, @PathVariable("zoteroGroupId") String zoteroGroupId, 
+            @PathVariable("itemId") String itemId, @RequestParam(required = false, value = "index") String index, 
+            @RequestParam(defaultValue = "1", required = false, value = "page") int page,
+            @RequestParam(value="collectionId", required=false) String collectionId,
+            @RequestParam(defaultValue = "title", required = false, value = "sortBy") String sortBy) 
+                    throws GroupDoesNotExistException, CannotFindCitationException, ZoteroHttpStatusException {
+        uploadChecker.add(itemId);
+        uploadChecker.checkUploads((IUser)authentication.getPrincipal(), zoteroGroupId);
         return "redirect:/auth/group/{zoteroGroupId}/items/{itemId}?index=" + index +"&page="+page +"&sortBy="+sortBy +"&collectionId="+collectionId;
     }
 }
